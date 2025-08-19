47de45bad80d05efd285e3b4acbdd42e4666f376
Anima <112791721+Animadon@users.noreply.github.com>
2025-08-19 21:57:25 +0545

    Update Web.py with updatable steps runnig on intergrated terminal
    Run in intergrated terminal

--- Web.py
+++ Web.py
@@ -10,26 +10,13 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://codewithharry.com"
-
-# step =1 get the html
 r = requests.get(url)
+# step =1 get the html
 htmlContent = r.content	# r returns response so if we want the code we write r.content
-print(htmlContent)		# printing the code
-
-# step 2 : Parse the htmlContent
-soup = BeautifulSoup(htmlContent, 'html.parser')
-print(soup.prettify())   # ✅ added () here
-
-# step3 :HTML Tree traversal
-# commonly used types of objects in bs4
-# 1.Tag
-# 2.NavigableString
-# 3.BeautifulSoup
-# 4.Comment
-
-#Get the title of the HTML page
+# print(htmlContent)
+#step 2
+soup = BeautifulSoup(r.content, 'html.parser')
+# print(soup.prettify())	# to print html in tree structure
+	# printing the code
 title = soup.title
-
-# Get all the paragraph from the page
-paras = soup.find_all('p')
-print(paras)
+print(type(title.string))	# to print the title of the page in string format
